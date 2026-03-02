c4336f8dfc8030db004f5414411673542318f9c5
Pankaj Sharma <pankajsharma0508@hotmail.com>
2026-03-02 17:54:08 +0100

    Update TrinoOauthClientSecretAuth.cs

--- trino-csharp/Trino.Client.Auth/TrinoOauthClientSecretAuth.cs
+++ trino-csharp/Trino.Client.Auth/TrinoOauthClientSecretAuth.cs
@@ -76,7 +76,10 @@ namespace Trino.Client.Auth
 
         private class TokenResponse
         {
+            [Newtonsoft.Json.JsonProperty("access_token")]
             public string AccessToken { get; set; }
+            
+            [Newtonsoft.Json.JsonProperty("expires_in")]
             public int ExpiresIn { get; set; }
         }
     }
